8b480f92a362caf3fe894169d5c1ca51f8fc02a2
Seweryn91 <34771956+Seweryn91@users.noreply.github.com>
2018-02-10 20:51:39 +0100

    add 'solve2' function

--- Day2/day2.py
+++ Day2/day2.py
@@ -17,6 +17,20 @@ def day_2_solve1(data):
     for row in data:
         checksum += (max(row) - min(row))
 
-    print("The solution is:", checksum)
+    print("The solution 1 is:", checksum)
+    
+def day_2_solve2(data):
+    '''solves second puzzle'''
+
+    data = [[int(numbers) for numbers in row.split()] for row in data.splitlines()]
+    checksum = 0
+
+    for row in data:
+        for i in row:
+            for j in row:
+                if i%j == 0 and i!=j:
+                    checksum += int(i/j)
+    print("The solution 2 is:", checksum)
 
 day_2_solve1(loader("day2.txt"))
+day_2_solve2(loader("day2.txt"))

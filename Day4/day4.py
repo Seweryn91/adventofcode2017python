aaf3bd4d20464d2d8699ce6c341c8dfa2834a65d
Seweryn91 <34771956+Seweryn91@users.noreply.github.com>
2018-02-10 22:46:06 +0100

    add solution for part2, fix loader

--- Day4/day4.py
+++ Day4/day4.py
@@ -1,6 +1,6 @@
 #day4
 
-def loader(file):
+def loader(file = "day4.txt"):
     '''loads words from inputfile'''
 
     with open(file) as rawfile:
@@ -16,5 +16,33 @@ def day4_solve1(data):
 			count += 1
 
 	print("Solution 1 is:",count)
+	
+	
+def day4_solve2(data):
+	'''solves second part checking each line (list).
+	If line is valid it increments value of "count" variable'''
+	data = [row.split() for row in data.splitlines()]
+	count = 0
+	for word in data:
+		if is_line_valid(word):
+			count += 1
+	print("Number of valid non-anagram passphrases:", count)
 
-day4_solve1(loader("day4.txt"))
+def is_line_valid(word):
+	'''checks line (list) for unique words apperance.
+	example: for first line:
+	0(index1) vxjtwn(word1) 1(index2) vjnxtw(word2)
+	word1 and word2 are anagrams therefore are invalid = line returns False'''
+	for index1, word1 in enumerate(word):
+		for index2, word2 in enumerate(word):
+			if index1 != index2 and are_words_anagrams(word1, word2):
+				return False
+	return True
+	
+
+def are_words_anagrams(word1, word2):
+	'''checks if words are of the same lenght and contain the same letters'''
+	return sorted(word1) == sorted(word2)
+
+day4_solve1(loader())
+day4_solve2(loader())

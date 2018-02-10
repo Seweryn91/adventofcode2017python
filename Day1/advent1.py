3696b9a3e5ffc0fe5d1678c9668fe151408fbd22
Seweryn91 <34771956+Seweryn91@users.noreply.github.com>
2018-02-10 20:05:54 +0100

    add function2

--- Day1/advent1.py
+++ Day1/advent1.py
@@ -13,3 +13,20 @@ def advent_day_1_num_1():
     print("First solution: ", solution)
 
 advent_day_1_num_1()
+
+def advent_day_1_num_2():
+    '''Second: halves the input and checks if those tho halves match, if so adds them'''
+
+    captcha = input("Type or paste captcha here for 2nd solution: ").strip()
+    solution2 = sum(d for i, d in enumerate(digit_list) if d == digit_list[(i+n//2) % n])
+        
+    if len(captcha) % 2 == 0:
+        digit_list = [int(digit) for digit in captcha]
+        n = len(digit_list)
+        
+    print("Second solution: ", solution2)
+    else:
+        print("Provided input lenght is odd. Calculated using middle number as halving point" , solution2)
+
+
+advent_day_1_num_2()
